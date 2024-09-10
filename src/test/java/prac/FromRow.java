51cca4516d67ab05989579a813663a9ccfe5837d
sanketgawai <127535799+sanketgawai@users.noreply.github.com>
2024-09-10 15:37:14 +0530

    change by other  person

--- src/test/java/prac/FromRow.java
+++ src/test/java/prac/FromRow.java
@@ -3,10 +3,10 @@ package prac;
 import java.io.IOException;
 
 public class FromRow {
-
+	
 	public static void main(String[] args) throws IOException {
 		
-		
+		System.out.println("change from github");
 		
 	}
 	
